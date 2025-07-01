c71dc5c224f8363f31eab7849211081daa05fa10
Gabriel Wright-Swaine <gabriel.wrightswaine184@gmail.com>
2025-07-01 13:45:51 -0400

    init commit

---
+++ src/main/java/NFLSpotlight/NflSpotlightApplication.java
@@ -0,0 +1,13 @@
+package NFLSpotlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NflSpotlightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NflSpotlightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/NFLSpotlight/NflSpotlightApplicationTests.java
@@ -0,0 +1,13 @@
+package NFLSpotlight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NflSpotlightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
